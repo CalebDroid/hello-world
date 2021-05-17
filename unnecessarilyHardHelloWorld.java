23053f996208c5685bc76d0ad0323e880f50d9ce
CalebDroid <74521445+CalebDroid@users.noreply.github.com>
2021-05-16 23:09:54 -0400

    Create unnecessarilyHardHelloWorld.java

---
+++ unnecessarilyHardHelloWorld.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;  //Importing class JFrame
+import javax.swing.JLabel;  //Importing class JLabel
+
+public class unnecessaryHardHelloWorld {
+   
+	public static void main(String[] args) {
+        
+		JFrame frame = new JFrame();           	//Creating frame
+        
+        frame.setTitle("Hi!");                 	//Setting title frame
+        frame.add(new JLabel("Hello, world!"));	//Adding text to frame
+        frame.pack();                          	//Setting size to smallest
+        frame.setLocationRelativeTo(null);     	//Centering frame
+        frame.setVisible(true);                	//Showing frame
+        frame.setSize(100, 100);			   	//Setting resolution
+    }
+}
